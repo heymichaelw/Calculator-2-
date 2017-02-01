8d0f923b006c470a9655eaf5bcb9654fbd210743
heymichaelw <heymichaelw@gmail.com>
2017-02-01 15:32:32 -0500

    Starting on stylesheet

---
+++ Calculator/Default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <div class="container">
+        <h1>Calculator</h1>
+        
+        <form id="form1" runat="server">
+            <div class="form-group">
+                <input type="number" class="form-control"  name="number1" step="any" />
+
+                <select name="Operation">
+                    <option>---</option>
+                    <option>+</option>
+                    <option>-</option>
+                    <option>/</option>
+                    <option>x</option>
+                </select>
+
+                <input type="number" class="form-control" name="number2" step="any" />
+
+                
+
+
+                <div>
+                    <input type="submit" class="btn btn-default" value="Calculate" />
+                </div>
+                <div>
+                    <h2><%= message %></h2>
+                    <h2><%= Equation %></h2>
+                    <h2><%= Calculation %></h2>
+                </div>
+            </div>
+        </form>
+    </div>
+
+</body>
+</html>
---
+++ Calculator/Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    public double Calculation;
+    public string operation;
+    public double number2;
+    public double number1;
+    public string message;
+    public string Equation;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(IsPostBack)
+        {
+            operation = Request.Form["Operation"];
+            number2 = double.Parse(Request.Form["number2"]);
+            number1 = double.Parse(Request.Form["number1"]);
+
+            
+
+            switch(operation)
+            {
+                case "+":
+                    Calculation = (double.Parse(Request.Form["number1"])) + (double.Parse(Request.Form["number2"]));
+                    break;
+                case "-":
+                    Calculation = (double.Parse(Request.Form["number1"])) - (double.Parse(Request.Form["number2"]));
+                    break;
+                case "/":
+                    if (number2 == 0)
+                    {
+                        message = "You cannot divide by zero!";
+                        break;
+                    }
+                    else
+                    {
+                        Calculation = (double.Parse(Request.Form["number1"])) / (double.Parse(Request.Form["number2"]));
+                        break;
+                    }
+                case "x":
+                    Calculation = (double.Parse(Request.Form["number1"])) * (double.Parse(Request.Form["number2"]));
+                    break;
+                default:
+                    break;
+            }
+
+            Equation = $"{number1} {operation} {number2} = {Calculation}";
+
+
+
+        }
+    }
+}
